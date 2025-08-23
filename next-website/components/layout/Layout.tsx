ab049ccf390c183bceae4163403d741d5ba45f7e
AnimeshRy <ani16september@gmail.com>
2025-08-23 17:07:13 +0530

    enable analytics

--- next-website/components/layout/Layout.tsx
+++ next-website/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,6 +13,7 @@ const Layout = ({ children }: LayoutProps) => {
       <Header />
       <main className="flex-grow">
         {children}
+        <Analytics />
       </main>
       <Footer />
     </div>
